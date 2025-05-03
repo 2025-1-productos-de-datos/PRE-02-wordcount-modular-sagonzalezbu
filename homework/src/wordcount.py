c9f2fe04ba0edf98c98381eed01b71cc540921dd
sagonzalezbu <sagonzalezbu@unal.edu.co>
2025-05-03 10:14:04 -0500

    move func to internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,6 +1,8 @@
 # obtain a list of files in the input directory
 import os
 
+from ._internals.write_count_words import write_count_words
+
 
 def read_all_lines():
     all_lines = []
@@ -13,8 +15,15 @@ def read_all_lines():
 
 
 def main():
+
     imput_file_list = os.listdir("data/input/")
     # all_lines = read_all_lines()
+
+    ## read all lines
+    ## preprocess lines
+    ## split in words
+    ## count words
+    ## write count words
 
     # count the frequency of the words in the files in the input directory
     counter = {}
@@ -27,17 +36,6 @@ def main():
 
     # create the directory output/ if it doesn't exist
     write_count_words(counter)
-
-
-def write_count_words(counter):
-    if not os.path.exists("data/output"):
-        os.makedirs("data/output")
-
-    # save the results using tsv format
-    with open("data/output/results.tsv", "w", encoding="utf-8") as f:
-        for key, value in counter.items():
-            # write the key and value to the file
-            f.write(f"{key}\t{value}\n")
 
 
 if __name__ == "__main__":
